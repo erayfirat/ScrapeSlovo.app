3140d3baa8ec4230f7f97a424c75ff7e9c11c76c
erayfirat <firateray@gmail.com>
2024-06-11 23:50:44 +0200

    refactoring and csv file out

--- scrape1.py
+++ scrape1.py
@@ -15,18 +15,26 @@ def GetSentence(verb):
     
     
     firstTable =[]
-    secondTable =[]
-    line=[]
+    
+    subject=[]
+    presensVerbum=[]
+    imperfectumVerbum=[]
+    perfectumVerbum=[]
     
     for td in soup.find_all('td'):
         firstTable.append(td.text);
-    
+        
     for i in range(0, len(firstTable)):
-        line.append(firstTable[i])
-        if(i%4==3):
-            secondTable.append(' '.join(line))
-            line=[]
-    
+        if(i%4==0):
+            subject.append(firstTable[i])
+        elif(i%4==1):
+            presensVerbum.append(firstTable[i])
+        elif(i%4==2):
+            imperfectumVerbum.append(firstTable[i])
+        elif(i%4==3):
+            perfectumVerbum.append(firstTable[i])
+ 
+
     liList=soup.find_all('li')
     liLen=len(liList)
     verblist=[]
@@ -42,17 +50,14 @@ def GetSentence(verb):
             perfectum.append(liList[x].text)
             verblist.append(verb)
 
+   
     
     presens.append('')
     imperfectum.append('')           
     perfectum.append('')
     verblist.append(verb)
-    # print(len(verblist))
-    # print(len(secondTable))
-    # print(len(presens))
-    # print(len(imperfectum))
-    # print(len(perfectum))
-    df=pd.DataFrame({'Verb':verblist,'General':secondTable, 'Presens':presens,'Imperfectum':imperfectum,'Perfectum':perfectum})
+
+    df=pd.DataFrame({'Verb':verblist,'Subject':subject,'PresensVerbum':presensVerbum,'ImperfectumVerbum':imperfectumVerbum,'PerfectumVerbum':perfectumVerbum, 'Presens':presens,'Imperfectum':imperfectum,'Perfectum':perfectum})
     # print(verb)
     return df
     
@@ -65,14 +70,14 @@ my_file = open("verbum0.txt", "r")
 data = my_file.read() 
   
 # replacing end splitting the text  
-# when newline ('\n') is seen. 
 data_into_list = data.split("\n") 
 # print(data_into_list) 
 my_file.close() 
 
 result=[]
-for verb in data_into_list:
-    
+# example
+# result.append(GetSentence('beginnen'))
+for verb in data_into_list:    
     try:
         result.append(GetSentence(verb))
     except Exception:
@@ -81,29 +86,5 @@ for verb in data_into_list:
 
 
 df=pd.concat(result)
-# verb='praten'
 
-#df1=pd.DataFrame(result,columns=['verb','Presens','Imperfectum','Perfectum','-'])
-
-# sentences=[]
-# for row in soup.select('table.lister-list tr'):
-#     title = row.find('td', class_='titleColumn').find('a').get_text()
-#     year = row.find('td', class_='titleColumn').find('span', class_='secondaryInfo').get_text()[1:-1]
-#     rating = row.find('td', class_='ratingColumn imdbRating').find('strong').get_text()
-#     sentences.append([title, year, rating])
-
-
-
-# Extract the relevant information from the HTML code
-# movies = []
-# for row in soup.select('tbody.lister-list tr'):
-#     title = row.find('td', class_='titleColumn').find('a').get_text()
-#     year = row.find('td', class_='titleColumn').find('span', class_='secondaryInfo').get_text()[1:-1]
-#     rating = row.find('td', class_='ratingColumn imdbRating').find('strong').get_text()
-#     movies.append([title, year, rating])
-
-# # Store the information in a pandas dataframe
-# df = pd.DataFrame(movies, columns=['Title', 'Year', 'Rating'])
-
-# # Add a delay between requests to avoid overwhelming the website with requests
-# time.sleep(1)
+df.to_csv('out.csv', index=False)  
